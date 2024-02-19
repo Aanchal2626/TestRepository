f1507ef0df9f2a8361d5932b4545cc1116563bd6
Jatin Sharma <jatinsharma4586@gmail.com>
2024-02-19 07:26:17 +0530

    changes

--- app/controllers/sites.controller.js
+++ app/controllers/sites.controller.js
@@ -55,16 +55,28 @@ userController.saveFolder = async (req, res) => {
             return res.send({ status: 0, msg: "Invalid Folder Code" });
         }
 
+        inputs.folder_name = inputs.folder_site_name + " - " + inputs.folder_name;
+
         const query = `
-        INSERT INTO sites (site_name, site_parent_id, site_code)
-        VALUES ($1, $2, $3)
+        INSERT INTO sites (site_name, site_parent_id, site_code, site_prefix, site_record_value)
+        VALUES ($1, $2, $3, $4, $5)
         ON CONFLICT (site_code)
         DO UPDATE SET
-          site_name = EXCLUDED.site_name
-      `;
+          site_name = EXCLUDED.site_name,
+          site_prefix = EXCLUDED.site_prefix,
+          site_record_value = EXCLUDED.site_record_value
+        RETURNING *;`;
 
-        let updatedSite = await pool.query(query, [inputs.site_name, 0, inputs.site_code]);
+        let updatedSite = await pool.query(query, [inputs.folder_name, inputs.folder_site, inputs.folder_code, inputs.folder_prefix, inputs.folder_records]);
+        updatedSite = updatedSite?.rows[0];
+
+        console.log(inputs);
+
         if (updatedSite) {
+            await pool.query('DELETE FROM users_sites_junction WHERE usj_site_id = $1', [updatedSite.site_id]);
+            for (let userId of inputs.user_permissions) {
+                await pool.query('INSERT INTO users_sites_junction (usj_user_id, usj_site_id) VALUES ($1, $2)', [userId, updatedSite.site_id]);
+            }
             res.send({ status: 1, msg: "Site saved successfully" });
         } else {
             res.send({ status: 0, msg: "Something went wrong" });

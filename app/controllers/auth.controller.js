f1a286916f216b9802027f8412d5cd6e71155474
Jatin Sharma <jatinsharma4586@gmail.com>
2024-02-28 04:23:08 +0530

    changes

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const { pool } = require("../helpers/database.helpers");
 const moment = require("moment");
+const nodemailer = require('nodemailer');
 const authController = {};
 
 authController.sendResetOTP = async (req, res) => {
@@ -7,7 +8,7 @@ authController.sendResetOTP = async (req, res) => {
         let inputs = req.body;
 
         if (!inputs.user_email) {
-            return res.send({ status: 0, msg: "Invalid Email" })
+            return res.send({ status: 0, msg: "Invalid Email" });
         }
 
         let result = await pool.query(`SELECT * FROM users WHERE user_email = $1`, [inputs.user_email]);
@@ -19,17 +20,53 @@ authController.sendResetOTP = async (req, res) => {
         let user = result.rows[0];
 
         if (!user.user_status) {
-            return res.send({ status: 0, msg: "User Suspended" })
+            return res.send({ status: 0, msg: "User Suspended" });
+        }
+
+        let currentTime = moment().unix();
+        if (user.user_otp_expire && currentTime < user.user_otp_expire) {
+            return res.send({ status: 1, msg: "OTP Still Valid" });
         }
 
         let otpExpireTimestamp = moment().add(5, 'minutes').unix();
+        let otp = Math.floor(100000 + Math.random() * 900000);
 
-        console.log(otpExpireTimestamp)
+        const transporter = nodemailer.createTransport({
+            host: 'mail.spsingla.com',
+            port: 465,
+            secure: true,
+            auth: {
+                user: "portal@spsingla.com",
+                pass: "New$pas"
+            }
+        });
+
+        const mailOptions = {
+            from: 'portal@spsingla.com',
+            to: user.user_email,
+            subject: 'SPS DOCS CONTROLLER PASSWORD RESET',
+            text: `TEST MAIL Your password reset OTP is: ${otp}`,
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log('Error sending email: ' + error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+
+        await pool.query(
+            `UPDATE users SET user_otp = $1, user_otp_expire = $2 WHERE user_id = $3`,
+            [otp, otpExpireTimestamp, user.user_id]
+        );
+
+        res.send({ status: 1, msg: "Sent OTP to Email" });
     } catch (err) {
-        console.log(err)
+        console.log(err);
         res.send({ status: 0, msg: "Something Went Wrong" });
     }
-}
+};
 
 authController.login = async (req, res) => {
     try {
